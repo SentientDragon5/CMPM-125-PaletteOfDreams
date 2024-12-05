34d6f1c57a1508043c59dbf723898543e2696401
carlosnuno1 <150867782+carlosnuno1@users.noreply.github.com>
2024-12-05 01:09:26 -0800

    last elevator not fixed

---
+++ Assets/Scripts/Last elevator trigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lastelevatortrigger : MonoBehaviour
+{
+    void OnTriggerEnter()
+    {
+        CombatManager.Instance.OnExitBattle();
+        // end game
+        GameObject g = new GameObject("DIED"); // Rename to WIN for win
+        print("died, created object");
+        DontDestroyOnLoad(g);
+        SceneManager.LoadScene("MainMenu");
+        // done
+    }
+}
